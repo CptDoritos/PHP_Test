bfd0df025398430cce50c0cff1a1863699f7500c
CptDoritos <fred.vezina@hotmail.com>
2023-04-20 20:37:26 -0400

    Controller with Message Methods

---
+++ phptest/app/Http/Controllers/PageController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use Illuminate\Http\Response;
+use App\Models\Users;
+use App\Models\Messages;
+
+
+class PageController extends Controller
+{
+    //
+    public function getMessages(){
+        $arrayMessages = Messages::all();
+        return response($arrayMessages, 201);
+    }
+
+    public function getMessage($search){
+        $arrayMessages = Messages::where('contents', 'LIKE', '%'.$search.'%')
+                        ->get();
+        return response($arrayMessages, 201);
+    }
+
+    public function getMessagesByUsers($search){
+        $users = Users::where('username', 'LIKE', '%'.$search.'%')
+			->orWhere('name', 'LIKE', '%'.$search.'%')
+			->get();
+        $items=[];
+        if(sizeof($user)>0){
+            $idUser = $user[0]->id;
+            $messages = Users::find($idUser)->users;
+        }
+
+        foreach ($users as $user) {
+            // ...
+        }
+        return response($users, 201);
+    }
+}
